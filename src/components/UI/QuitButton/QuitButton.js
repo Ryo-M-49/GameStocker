0a43dde2d1a360010b495acafdde4ea71860c644
Ryosuke <ryo.m.code@gmail.com>
2020-01-14 23:46:26 +0900

    implemented goBack Button Component

---
+++ src/components/UI/QuitButton/QuitButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './QuitButton.module.css';
+import {withRouter} from 'react-router';
+
+const quitButton = (props) => (
+    <div 
+        className={classes.QuitButton}
+        onClick={() => props.history.goBack()}
+        >
+        <i class="fa fa-times-circle"></i>
+    </div>
+);
+
+
+export default withRouter(quitButton);

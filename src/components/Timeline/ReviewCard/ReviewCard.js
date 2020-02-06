b2f6417d7d4c540be96306bf4d6e41979e890c8f
Ryosuke <ryo.m.code@gmail.com>
2020-02-06 21:27:11 +0900

    Create ReviewCard.js

---
+++ src/components/Timeline/ReviewCard/ReviewCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './ReviewCard.module.css';
+
+const reviewCard = props => (
+    <div className={classes.ReviewCard}>
+        <div>
+            test
+        </div>
+    </div>
+);
+
+export default reviewCard;

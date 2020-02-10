65e0b3caf0a904a303ed30ee73518b7cd3eda25a
Ryo-M-49 <ryo.m.code@gmail.com>
2020-02-10 07:13:45 +0000

    Add MuiBurron

--- src/components/UI/SaveButton/SaveButton.js
+++ src/components/UI/SaveButton/SaveButton.js
@@ -1,6 +1,23 @@
 import React from 'react';
-import classes from './SaveButton.module.css';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+// The `withStyles()` higher-order component is injecting a `classes`
+// prop that is used by the `Button` component.
+const StyledButton = withStyles({
+    root: {
+        width: '200px',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+})(Button);
 
-const saveButton = props => <div className={classes.SaveButton} />;
+const saveButton = props => (
+    <div>
+        <StyledButton variant="contained" color="primary">
+            Save
+        </StyledButton>
+    </div>
+);
 
 export default saveButton;
--- src/components/UI/ShareButton/ShareButton.js
+++ src/components/UI/ShareButton/ShareButton.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import classes from './ShareButton.module.css';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const StyledButton = withStyles({
+    root: {
+        width: '200px',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+})(Button);
 
 const shareButton = props => (
-    <div className={classes.ShareButton}>
-        <button>Share with the World</button>
+    <div>
+        <StyledButton variant="contained" color="primary">
+            Share
+        </StyledButton>
     </div>
 );
 

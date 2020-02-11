f0282282c81ccbe236aba23d1fac44e0f608d2c9
Ryosuke <ryo.m.code@gmail.com>
2020-02-11 21:57:46 +0900

    Modify Review component

--- src/components/YourReviews/YourReview/YourReview.js
+++ src/components/YourReviews/YourReview/YourReview.js
@@ -7,20 +7,12 @@ import SaveButton from '../../UI/SaveButton/SaveButton';
 
 const yourReview = props => {
     const game = props.location.aboutProps.game;
-    let description = game.itemCaption;
-    if (description.length > 400) {
-        description = description.substr(0, 400) + '...';
-    }
 
     return (
         <div className={classes.YourReview}>
             <QuitButton />
             <div className={classes.ReviewWrapper}>
-                <div className={classes.Description}>
-                    <h2 className={classes.Title}>{game.title}</h2>
-                    <p className={classes.Description}>{description}</p>
-                </div>
-                <Review />
+                <Review game={game} />
             </div>
             <div className={classes.RightContent}>
                 <div className={classes.ImageWrapper}>

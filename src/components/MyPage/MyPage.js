0ffa7918485c64aae793d46ca82a895c96b03498
Ryo-M-49 <ryo.m.code@gmail.com>
2020-02-13 23:56:08 +0000

    Add ToAllReviewsButton to MyPage

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './MyPage.module.css';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import YourPicture from './YourPicture/YourPicture';
 import Bio from './Bio/Bio';
 import PopularReview from './PopularReview/PopularReview';
-// import ToAllReviewsButton from '../UI/ToAllReviewsButton/ToAllReviewsButton';
+import ToAllReviewsButton from '../UI/ToAllReviewsButton/ToAllReviewsButton';
 import RecentActivity from './RecentActivity/RecentActivity';
 
 const myPage = props => (
@@ -16,9 +16,9 @@ const myPage = props => (
             </div>
             <PopularReview />
             <div className={classes.ToAllReviewsButtonWrapper}>
-                {/* <Link to='/yourreview'>
+                <Link to='/yourreviews'>
                     <ToAllReviewsButton />
-                </Link> */}
+                </Link>
             </div>
         </div>
         <div className={classes.MyPageRight}>

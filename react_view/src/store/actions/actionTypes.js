adc94dc3f03c29f32a7280ced729b9f578c3bbec
Ryosuke <ryo.m.code@gmail.com>
2020-02-24 22:58:43 +0900

    Replace the state management with Redux in GameList.js

--- react_view/src/store/actions/actionTypes.js
+++ react_view/src/store/actions/actionTypes.js
@@ -1,5 +1,5 @@
 // for ./gameList.js
-export const UPDATE_GAMES = 'UPDATE_GAMES';
-export const UPDATE_CURRENT_PAGE = 'UPDATE_CURRENT_PAGE';
 export const SET_GAMES = 'SET_GAMES';
+export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+export const UPDATE_GAMES = 'UPDATE_GAMES';
 export const FETCH_GAMES_FAILED = 'FETCH_GAMES_FAILED';

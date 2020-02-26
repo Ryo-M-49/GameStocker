ad9c91b9389b5b7124aac81d4a9495829b680ab5
Ryosuke <ryo.m.code@gmail.com>
2020-02-27 00:40:40 +0900

    Add auth-related pages

--- react_view/src/hoc/Layout/Layout.js
+++ react_view/src/hoc/Layout/Layout.js
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom';
 import Aux from '../Aux/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import YourReviews from '../../components/YourReviews/YourReviews';
+import Signin from '../../components/Authentication/Signin';
+import Signup from '../../components/Authentication/Signup';
 import GameList from '../../containers/GameList/GameList';
 import YourReview from '../../components/YourReviews/YourReview/YourReview';
 import MyPage from '../../components/MyPage/MyPage';
@@ -45,6 +47,8 @@ class Layout extends Component {
                             exact
                             component={YourReviews}
                         />
+                        <Route path="/signin" exact component={Signin} />
+                        <Route path="/signup" exact component={Signup} />
                         <Route path="/gamelist" exact component={GameList} />
                         <Route path="/mypage" component={MyPage} />
                         <Route path="/:id" exact component={YourReview} />

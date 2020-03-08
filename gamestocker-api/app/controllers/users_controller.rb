9e8d0e42823b570b55a69554c28187950b3d77a9
Ryosuke <ryo.m.code@gmail.com>
2020-03-08 11:47:07 +0900

    Change port to connect from 3000 to 3001

--- gamestocker-api/app/controllers/users_controller.rb
+++ gamestocker-api/app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
-    def index
-        @user = User.all
-        render json: @user
-    end
+  def index
+    @user = User.all
+    render json: @user
+  end
 
-    def create
-        @user = User.create(params[:user])
-        render json: @user 
-    end
+  def create
+    @user = User.create(params[:user])
+    render json: @user
+  end
 
-    private
+  private
 
-        def user_params
-            params.require(:user).permit(:fist_name, :last_name, :email, :password, :password_confirmation)
-        end
-    
+  def user_params
+    params.require(:user).permit(:fist_name, :last_name, :email, :password, :password_confirmation)
+  end
 end

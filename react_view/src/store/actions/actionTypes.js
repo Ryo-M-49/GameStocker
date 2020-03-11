3f596f0edb99edbd007570ac2b7247779f84c02e
Ryosuke <ryo.m.code@gmail.com>
2020-03-11 21:40:46 +0900

    Complete the action file of user-related functions

--- react_view/src/store/actions/actionTypes.js
+++ react_view/src/store/actions/actionTypes.js
@@ -18,10 +18,9 @@ export const SIGNUP_FAIL = 'SIGNUP_FAIL';
 export const SET_SIGNUP_REDIRECT_PATH = 'SET_SIGNUP_REDIRECT_PATH';
 
 // for ./user.js
+export const CONTROL_USER_FAIL = 'GET_USER_FAIL';
 export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
-export const GET_USER_FAIL = 'GET_USER_FAIL';
 export const GET_USER = 'GET_USER';
 export const EDIT_USER = 'EDIT_USER';
+export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
 export const DELETE_USER = 'DELETE_USER';
-export const CHANGE_PASSWORD = 'CHANGE_PASSWORD';
-export const CHECK_CURRENT_USER = 'CHECK_CURRENT_USER';

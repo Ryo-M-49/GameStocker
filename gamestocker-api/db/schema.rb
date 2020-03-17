e9c078d6edaf48db6318eff9ba3cdfc702a86c48
Ryosuke <ryo.m.code@gmail.com>
2020-03-17 20:09:18 +0900

    Add Post model and controller

--- gamestocker-api/db/schema.rb
+++ gamestocker-api/db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_133919) do
+ActiveRecord::Schema.define(version: 2020_03_17_075253) do
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.text "good"
+    t.text "bad"
+    t.float "rate"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_reviews_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -38,4 +49,5 @@ ActiveRecord::Schema.define(version: 2020_03_11_133919) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end

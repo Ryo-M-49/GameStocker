bc38c15ec917c0931b24886464b2289a5b495ba4
Ryosuke <ryo.m.code@gmail.com>
2020-03-17 20:09:18 +0900

    Add Post model and controller

--- gamestocker-api/app/controllers/application_controller.rb
+++ gamestocker-api/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email])
   end
 
   include DeviseTokenAuth::Concerns::SetUserByToken

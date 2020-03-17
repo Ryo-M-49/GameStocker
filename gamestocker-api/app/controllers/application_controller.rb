0100c86463df3e5c41c4973fdb1c9a5b02e39039
Ryosuke <ryo.m.code@gmail.com>
2020-03-17 14:23:12 +0900

    Fix the issue where POST in signing up does not work because of inproper data thrown

--- gamestocker-api/app/controllers/application_controller.rb
+++ gamestocker-api/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
   end
 
   include DeviseTokenAuth::Concerns::SetUserByToken

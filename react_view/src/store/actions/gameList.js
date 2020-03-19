9c85eea718820d10dde7132055a95c01dfedc6fd
Ryo-M-49 <ryo.m.code@gmail.com>
2020-03-19 01:50:07 +0000

    Add the action creator and reducer to fetch all games

--- react_view/src/store/actions/gameList.js
+++ react_view/src/store/actions/gameList.js
@@ -8,6 +8,14 @@ export const setGames = games => {
     };
 };
 
+export const setAllGames = allGames => {
+    return {
+        type: actionTypes.SET_ALL_GAMES,
+        allGames: allGames,
+        isSearched: true
+    }
+}
+
 export const setCurrentPage = selectedPage => {
     return {
         type: actionTypes.SET_CURRENT_PAGE,
@@ -19,6 +27,23 @@ export const fetchGamesFailed = () => {
     return {
         type: actionTypes.FETCH_GAMES_FAILED,
     };
+};
+
+export const fetchAllGames = (pageCount) => {
+    const games = [];
+    for (let page = 1; page <= pageCount; page++) {
+        axios
+            .get(
+                `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&page=${page}&hits=30&booksGenreId=006&applicationId=1009084489441242376`
+            )
+            .then(response => {
+                games.push(response.data);
+            })
+            .catch(error => {
+                console.log('Failed to fetch all games');
+            });
+    };
+    return games;
 };
 
 export const updateGamesByPage = currentPage => {

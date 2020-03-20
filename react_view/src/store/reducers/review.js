1dc068a58e99bd8d28f3247dc04e49ea0df5d344
Ryosuke <ryo.m.code@gmail.com>
2020-03-21 01:47:45 +0900

    Implement the function to add the game state to store when YourReview comonent is loaded

--- react_view/src/store/reducers/review.js
+++ react_view/src/store/reducers/review.js
@@ -2,9 +2,14 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from './utility';
 
 const initialState = {
-    review: {
+    game: {
+        id: null,
         title: null,
+        caption: null,
         image: null,
+        gameUrl: null,
+    },
+    review: {
         good: null,
         bad: null,
         rate: 3  
@@ -17,10 +22,18 @@ const setReview = (state, action) => {
     });
 };
 
+const setGame = (state, action) => {
+    return updateObject(state, {
+        game: action.game,
+    });
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.SET_GAMES:
+        case actionTypes.SET_REVIEW:
             return setReview(state, action);
+        case actionTypes.SET_GAME:
+            return setGame(state, action);
         default:
             return state;
     }

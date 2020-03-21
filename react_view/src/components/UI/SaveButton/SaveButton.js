f8ce422085fea8fb3c432a74c24f52b48fefd14c
Ryosuke <ryo.m.code@gmail.com>
2020-03-21 22:56:00 +0900

    Modify axios requests in the review action

--- react_view/src/components/UI/SaveButton/SaveButton.js
+++ react_view/src/components/UI/SaveButton/SaveButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import * as actions from '../../../../store/actions/index';
+import * as actions from '../../../store/actions/index';
 
 const StyledButton = withStyles({
     root: {
@@ -13,15 +13,30 @@ const StyledButton = withStyles({
     },
 })(Button);
 
-const saveButton = props => (
-    <div>
-        <StyledButton
-            variant="contained"
-            color="secondary"
-            >
-            Save
-        </StyledButton>
-    </div>
-);
+const SaveButton = props => {
+    const review = useSelector(state => state.reviewReducer.review);
+    const game = useSelector(state => state.reviewReducer.game);
+    const dispatch = useDispatch();
 
-export default saveButton;
+    const saveButtonClickedHandler = () => {
+        const updatedReview = {
+            ...review,
+            ...game
+        };
+        dispatch(actions.createReview(updatedReview, ));
+    };
+    
+    return (
+        <div>
+            <StyledButton
+                variant="contained"
+                color="secondary"
+                onClick={saveButtonClickedHandler}
+                >
+                Save
+            </StyledButton>
+        </div>
+    );
+};
+
+export default SaveButton;

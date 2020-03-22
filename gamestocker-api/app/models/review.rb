b0bcf688c8962fe5af0a23ff074f5cabf72a89c1
Ryosuke <ryo.m.code@gmail.com>
2020-03-22 22:14:14 +0900

    Fix the issue where the post request for a new review fails because of the wrong validation in the model

--- gamestocker-api/app/models/review.rb
+++ gamestocker-api/app/models/review.rb
@@ -2,7 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true
   validates :rate, presence: true
   validates :good, presence: true
   validates :bad, presence: true

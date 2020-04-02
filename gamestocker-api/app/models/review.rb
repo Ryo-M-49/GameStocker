e38a2273f36ce0194c7e88309f11c6f458f73e5e
Ryosuke <ryo.m.code@gmail.com>
2020-04-02 23:11:42 +0900

    Add Like model and controller

--- gamestocker-api/app/models/review.rb
+++ gamestocker-api/app/models/review.rb
@@ -1,5 +1,16 @@
 class Review < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
+
+  def like(user)
+    likes.create(user_id: user.id)
+  end
+
+  def unlike(user)
+    likes.find_by(user_id: user.id).destroy
+  end
+
 end

84a7b30eb4324db953cec6646d162c183a700343
Ryosuke <ryo.m.code@gmail.com>
2020-04-03 00:19:53 +0900

    Add create/delete actions for Like

--- gamestocker-api/app/controllers/likes_controller.rb
+++ gamestocker-api/app/controllers/likes_controller.rb
@@ -1,20 +1,24 @@
 class LikesController < ApplicationController
   before_action :set_user
+  before_action :set_user_review, only: [:create]
 
   def create
-    @review = Review.find(params[:review_id])
-    @review.like(user)
+    @review.like(@user)
     render json: @review
   end
 
   def destroy
     @review = Like.find(params[:id]).review
-    @review.unlike(user)
+    @review.unlike(@user)
+    render json: @review
   end
 
   private
   def set_user
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 
+  def set_user_review
+    @review = @user.reviews.find_by(gameId: params[:review_id])
+  end
 end

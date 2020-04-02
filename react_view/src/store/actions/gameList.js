09e3f5363f97324944945a7f5620725c936a418b
Ryo-M-49 <ryo.m.code@gmail.com>
2020-04-02 07:53:03 +0000

    Implement the search function

--- react_view/src/store/actions/gameList.js
+++ react_view/src/store/actions/gameList.js
@@ -10,15 +10,6 @@ export const setGames = games => {
     };
 };
 
-// Used to set searched games
-export const setSearchedGames = searchedGames => {
-    return {
-        type: actionTypes.SET_SEARCHED_GAMES,
-        searchedGames: searchedGames,
-        isSearched: true,
-    };
-};
-
 export const setCurrentPage = selectedPage => {
     return {
         type: actionTypes.SET_CURRENT_PAGE,
@@ -34,10 +25,9 @@ export const fetchGamesFailed = () => {
 
 export const updateGamesByPage = currentPage => {
     return dispatch => {
+        const url = `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&page=${currentPage}&hits=30&booksGenreId=006&applicationId=1009084489441242376`; 
         axios
-            .get(
-                `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&page=${currentPage}&hits=30&booksGenreId=006&applicationId=1009084489441242376`
-            )
+            .get(url)
             .then(response => {
                 console.log(response.data);
                 dispatch(setGames(response.data));
@@ -48,30 +38,19 @@ export const updateGamesByPage = currentPage => {
     };
 };
 
-export const sleep = time => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, time);
-    });
-};
-
-export const fetchAllGames = async (delay, pageCount) => {
-    const games = [];
-    for (let page = 1; page <= pageCount; page++) {
-        await axios
-            .get(
-                `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&page=${page}&hits=30&booksGenreId=006&applicationId=1009084489441242376`
-            )
+export const updateGamesByTitle = title => {
+    return dispatch => {
+        const url = `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&title=${title}&hits=30&booksGenreId=006&applicationId=1009084489441242376`;
+        axios
+            .get(url)
             .then(response => {
-                games.push(response.data);
-                return Promise.resolve(response);
+                console.log(url);
+                console.log(response.data);
+                dispatch(setGames(response.data));
             })
             .catch(error => {
-                console.log('Failed to fetch all games');
+                dispatch(fetchGamesFailed());
             });
-        await sleep(delay);
-    }
-    console.log(games);
-    return games;
+    };
 };
+

61ab12c4f2eb7823a745ccd7416f9d8abb19bb4f
Ryosuke <ryo.m.code@gmail.com>
2020-04-14 00:54:20 +0900

    Implement Read More button in SmallReviewCard component

--- react_view/src/components/MyPage/RecentActivity/RecentActivity.js
+++ react_view/src/components/MyPage/RecentActivity/RecentActivity.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './RecentActivity.module.css';
-import SmallReviewCard from '../../SmallReviewCard/SmallReviewCard';
+import SmallReviewCard from './SmallReviewCard/SmallReviewCard';
 
 const RecentActivity = props => {
     const { reviews } = props;

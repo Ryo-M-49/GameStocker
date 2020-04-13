4d3ba8ccbc0f42284a342f72c8ee4abcb3644e4f
Ryo-M-49 <ryo.m.code@gmail.com>
2020-04-13 02:32:32 +0000

    Implement the progress circle while fetching data from rakuten api

--- react_view/src/store/actions/gameList.js
+++ react_view/src/store/actions/gameList.js
@@ -8,6 +8,13 @@ export const setSearched = () => {
     };
 };
 
+export const setLoading = isLoading => {
+    return {
+        type: actionTypes.SET_LOADING,
+        isLoading: isLoading,
+    };
+};
+
 export const setGames = games => {
     return {
         type: actionTypes.SET_GAMES,
@@ -33,11 +40,13 @@ export const fetchGamesFailed = () => {
 
 export const updateGamesByPage = currentPage => {
     return dispatch => {
+        dispatch(setLoading(true));
         const url = `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&page=${currentPage}&hits=30&booksGenreId=006&applicationId=1009084489441242376`;
         axios
             .get(url)
             .then(response => {
                 dispatch(setGames(response.data));
+                dispatch(setLoading(false));
             })
             .catch(error => {
                 dispatch(fetchGamesFailed());
@@ -48,11 +57,13 @@ export const updateGamesByPage = currentPage => {
 export const updateGamesByTitle = title => {
     // setSearched();
     return dispatch => {
+        dispatch(setLoading(true));
         const url = `https://app.rakuten.co.jp/services/api/BooksGame/Search/20170404?format=json&hardware=PS&title=${title}&hits=30&booksGenreId=006&applicationId=1009084489441242376`;
         axios
             .get(url)
             .then(response => {
                 dispatch(setGames(response.data));
+                dispatch(setLoading(false));
             })
             .catch(error => {
                 dispatch(fetchGamesFailed());

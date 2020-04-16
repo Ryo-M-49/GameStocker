2099edee98c9ee81490efcc4cdc185665fc79d05
Ryosuke <ryo.m.code@gmail.com>
2020-04-17 01:23:11 +0900

    Delete axios request in MyPage

--- react_view/src/store/actions/actionTypes.js
+++ react_view/src/store/actions/actionTypes.js
@@ -24,7 +24,7 @@ export const SET_SIGNUP_REDIRECT_PATH = 'SET_SIGNUP_REDIRECT_PATH';
 
 // for ./user.js
 export const CONTROL_USER_FAIL = 'GET_USER_FAIL';
-export const SET_IMAGE_SUCCESS = 'SET_IMAGE_SUCCESS';
+export const SET_IMAGE = 'SET_IMAGE';
 export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
 export const GET_USER = 'GET_USER';
 export const EDIT_USER = 'EDIT_USER';
--- react_view/src/store/actions/user.js
+++ react_view/src/store/actions/user.js
@@ -20,26 +20,26 @@ export const getUserSuccess = userData => {
     };
 };
 
-export const setImageSuccess = image => {
+export const setImage = image => {
     return {
-        type: actionTypes.SET_IMAGE_SUCCESS,
+        type: actionTypes.SET_IMAGE,
         image: image,
     };
 };
 
-export const setImage = (image, userId) => {
-    return dispatch => {
-        const url = `http://localhost:3001/users/${userId}/upload_image`;
-        axios
-            .post(url, image)
-            .then(response => {
-                dispatch(setImageSuccess(response.data.image));
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
-};
+// export const setImage = (image, userId) => {
+//     return dispatch => {
+//         const url = `http://localhost:3001/users/${userId}/upload_image`;
+//         axios
+//             .post(url, image)
+//             .then(response => {
+//                 dispatch(setImageSuccess(response.data.image));
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//             });
+//     };
+// };
 
 export const getUser = userId => {
     return dispatch => {

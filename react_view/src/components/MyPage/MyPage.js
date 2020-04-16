de30cf1b8d2c3d85f2e157a4aee4ecb3d32fb88e
Ryosuke <ryo.m.code@gmail.com>
2020-04-16 22:42:15 +0900

    Implement Dropzone and form for receiving an image to upload

--- react_view/src/components/MyPage/MyPage.js
+++ react_view/src/components/MyPage/MyPage.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import {useDropzone} from 'react-dropzone'
 import { makeStyles } from '@material-ui/core/styles';
 import classes from './MyPage.module.css';
 import { Link } from 'react-router-dom';
 import Aux from '../../hoc/Aux/Aux';
 import EditButton from '../UI/EditButton/EditButton';
+import Button from '@material-ui/core/Button';
 import CancelButton from '../UI/CancelButton/CancelButton';
 import SaveButton from '../UI/SaveButton/SaveButton';
 import PopularReview from './PopularReview/PopularReview';
 import ToAllReviewsButton from '../UI/ToAllReviewsButton/ToAllReviewsButton';
 import RecentActivity from './RecentActivity/RecentActivity';
-import ProfileImage from '../../assets/images/default-user.png';
+import DefaultImage from '../../assets/images/default-user.png';
 import Avatar from '../UI/Avatar/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import * as actions from '../../store/actions/index';
@@ -83,12 +86,39 @@ const MyPage = props => {
             </div>
         </Aux>
     );
+
+    const onDrop = useCallback(acceptedFiles => {
+        if (acceptedFiles && acceptedFiles[0]) {
+            let formPayLoad = new FormData();
+            formPayLoad.append('uploaded_image', acceptedFiles[0]);
+            dispatch(actions.setImage(formPayLoad));
+        }
+      }, []);
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
     if (isEditing) {
         bio = (
             <Aux>
                 <div className={classes.BioWrapper}>
                     <div className={classes.Picture}>
-                        <Avatar image={ProfileImage} />
+                        <Avatar image={profileImage ? profileImage : DefaultImage} />
+                        <div className={classes.Dropzone} {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {
+                                isDragActive ?
+                                <Button
+                                    startIcon={<ImageIcon />}
+                                    variant='contained'
+                                >
+                                    DROP IMAGE
+                                </Button> :
+                                <Button
+                                    startIcon={<ImageIcon />}
+                                    variant='contained'
+                                >
+                                    UPLOAD
+                                </Button>
+                            }
+                        </div>
                         <div className={classes.Name}>
                             <TextField
                                 className={classStyles.name}

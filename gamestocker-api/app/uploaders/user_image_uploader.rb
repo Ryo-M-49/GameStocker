e4719567550b58000dd71d68094cc345ef938935
Ryosuke <ryo.m.code@gmail.com>
2020-04-29 23:39:24 +0900

    Prepare CarrierWave and AWS for image uploading function

--- gamestocker-api/app/uploaders/user_image_uploader.rb
+++ gamestocker-api/app/uploaders/user_image_uploader.rb
@@ -1,11 +1,15 @@
 class UserImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -28,10 +32,10 @@ class UserImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  Create different versions of your uploaded files:
+  version :thumb do
+    process resize_to_fit: [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:

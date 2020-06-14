593d854cc18f7613655df8ba26d750914bfb4e03
Ryosuke <ryo.m.code@gmail.com>
2020-06-14 23:44:18 +0900

    [FIX] Enable user to access review page without authenticated

--- react_view/src/components/YourReview/YourReview.js
+++ react_view/src/components/YourReview/YourReview.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Redirect } from 'react-router-dom';
 import classes from './YourReview.module.css';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
@@ -92,16 +91,8 @@ const YourReview = props => {
         );
     }
 
-    // Redirect if not authenticated
-    const isAuthenticated = auth.token !== null;
-    let authRedirect = null;
-    if (!isAuthenticated) {
-        authRedirect = <Redirect to="/signin" />;
-    }
-
     return (
         <div className={classes.YourReview}>
-            {authRedirect}
             {notification}
             <QuitButton />
             <div className={classes.ReviewWrapper}>
